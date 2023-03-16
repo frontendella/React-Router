bf440723b175d79347b40b7a73e2f4999d602fa2
frontendella <ryukhinaella@gmail.com>
2023-03-15 22:31:46 -0700

    Up one directory is implemented in Link for "HostVansDetail" file

--- pages/Host/HostVansDetail.jsx
+++ pages/Host/HostVansDetail.jsx
@@ -1,9 +1,7 @@
 import React from "react"
-import { useParams } from "react-router-dom"
+import { useParams, Link } from "react-router-dom"
 
 export default function HostVanDetail() {
-
-
     const { id } = useParams()
     const [currentVan, setCurrentVan] = React.useState(null)
 
@@ -12,17 +10,34 @@ export default function HostVanDetail() {
             .then(res => res.json())
             .then(data => setCurrentVan(data.vans))
     }, [])
-    
+
     if (!currentVan) {
         return <h1>Loading...</h1>
     }
 
+        
     return (
-        <div>
-            <img src={currentVan.imageUrl} width={150} />
-            <h2>{currentVan.name}</h2>
-            <p>{currentVan.price}</p>
-            <p>{currentVan.type}</p>
-        </div>
+        <section>
+            <Link
+                to=".."
+                relative="path"
+                className="back-button"
+            >&larr; <span>Back to all vans</span></Link>
+
+            <div className="host-van-detail-layout-container">
+                <div className="host-van-detail">
+                    <img src={currentVan.imageUrl} />
+                    <div className="host-van-detail-info-text">
+                        <i
+                            className={`van-type van-type-${currentVan.type}`}
+                        >
+                            {currentVan.type}
+                        </i>
+                        <h3>{currentVan.name}</h3>
+                        <h4>${currentVan.price}/day</h4>
+                    </div>
+                </div>
+            </div>
+        </section>
     )
 }
