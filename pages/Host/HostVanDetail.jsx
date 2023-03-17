e6528de895ee5ffe8f354ed904d76c1e0a7b848c
frontendella <ryukhinaella@gmail.com>
2023-03-16 18:50:02 -0700

    Added Outlet Context

--- pages/Host/HostVanDetail.jsx
+++ pages/Host/HostVanDetail.jsx
@@ -23,46 +23,50 @@ export default function HostVanDetail() {
     }
     return (
         <section>
-            <Link
-                to=".."
-                relative="path"
-                className="back-button"
-            >&larr; <span>Back to all vans</span></Link>
+        <Link
+            to=".."
+            relative="path"
+            className="back-button"
+        >&larr; <span>Back to all vans</span></Link>
 
-            <div className="host-van-detail-layout-container">
-                <div className="host-van-detail">
-                    <img src={currentVan.imageUrl} />
-                    <div className="host-van-detail-info-text">
-                        <i
-                            className={`van-type van-type-${currentVan.type}`}
-                        >
-                            {currentVan.type}
-                        </i>
-                        <h3>{currentVan.name}</h3>
-                        <h4>${currentVan.price}/day</h4>
-                    </div>
+        <div className="host-van-detail-layout-container">
+            <div className="host-van-detail">
+                <img src={currentVan.imageUrl} />
+                <div className="host-van-detail-info-text">
+                    <i
+                        className={`van-type van-type-${currentVan.type}`}
+                    >
+                        {currentVan.type}
+                    </i>
+                    <h3>{currentVan.name}</h3>
+                    <h4>${currentVan.price}/day</h4>
                 </div>
-                <nav className="host-van-detail-nav">
-                    <NavLink 
+            </div>
+
+            <nav className="host-van-detail-nav">
+                <NavLink
                     to="."
                     end
-                    style={({isActive})=> isActive ? activeStyles : null}
-                    >Details
-                    </NavLink>
-                    <NavLink 
+                    style={({ isActive }) => isActive ? activeStyles : null}
+                >
+                    Details
+                </NavLink>
+                <NavLink
                     to="pricing"
-                    style={({isActive})=> isActive ? activeStyles : null}
-                    >Pricing
-                    </NavLink>
-                    <NavLink 
+                    style={({ isActive }) => isActive ? activeStyles : null}
+                >
+                    Pricing
+                </NavLink>
+                <NavLink
                     to="photos"
-                    style={({isActive})=> isActive ? activeStyles : null}
-                    >Photos
-                    </NavLink>
-                </nav>
-                <Outlet />
-            </div>
-        </section>
+                    style={({ isActive }) => isActive ? activeStyles : null}
+                >
+                    Photos
+                </NavLink>
+            </nav>
+            <Outlet context={{currentVan}} />
+        </div>
+    </section>
     )
 }
 
