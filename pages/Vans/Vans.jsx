77c04baf5fb7b91ef41cf1e8e1be5d8b2adbce8c
frontendella <ryukhinaella@gmail.com>
2023-03-17 18:36:53 -0700

    Added conditional rendering to "Vans" file

--- pages/Vans/Vans.jsx
+++ pages/Vans/Vans.jsx
@@ -31,27 +31,28 @@ export default function Vans() {
         </div>
     ))
 
+
+
     return (
         <div className="van-list-container">
             <h1>Explore our van options</h1>
             <div className="van-list-filter-buttons">
                 <button 
                     onClick={() => setSearchParams({type: "simple"})}
-                    className="van-type simple"
+                    className={`van-type simple  ${typeFilter ==="simple" && "selected"}`}
                 >Simple</button>
                 <button 
                     onClick={() => setSearchParams({type: "luxury"})}
-                    className="van-type luxury"
+                    className={`van-type luxury ${typeFilter === "luxury" && "selected" }`}
                 >Luxury</button>
                 <button 
                     onClick={() => setSearchParams({type: "rugged"})}
-                    className="van-type rugged"
+                    className={`van-type rugged ${typeFilter === "rugged" && "selected"}`}
                 >Rugged</button>
-                <button 
+                {typeFilter && <button 
                     onClick={() => setSearchParams({})}
                     className="van-type clear-filters"
-                >Clear filter</button>
-            
+                    >Clear filter</button>}
             </div>
             <div className="van-list">
                 {vanElements}
