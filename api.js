d449c0a67699cba102a6ae029ecb987bc8037e39
frontendella <ryukhinaella@gmail.com>
2023-03-20 21:39:57 -0700

    Setup authentication

--- api.js
+++ api.js
@@ -1,6 +1,6 @@
 export async function getVans() {
     const res = await fetch("/api/vans")
-    if (!res.ok){
+    if (!res.ok) {
         throw {
             message: "Failed to fetch vans",
             statusText: res.statusText,
@@ -9,4 +9,21 @@ export async function getVans() {
     }
     const data = await res.json()
     return data.vans
+}
+
+export async function loginUser(creds) {
+    const res = await fetch("/api/login",
+        { method: "post", body: JSON.stringify(creds) }
+    )
+    const data = await res.json()
+
+    if (!res.ok) {
+        throw {
+            message: data.message,
+            statusText: res.statusText,
+            status: res.status
+        }
+    }
+
+    return data
 }
