bb2fdfec213dae04a64b16bdeb3709239ea08135
frontendella <ryukhinaella@gmail.com>
2023-03-20 17:56:02 -0700

    Added an errorElement

--- pages/Vans/Vans.jsx
+++ pages/Vans/Vans.jsx
@@ -2,38 +2,16 @@ import React from "react"
 import { Link, useSearchParams, useLoaderData } from "react-router-dom"
 import { getVans } from "../../api"
 
-export function loader(){
-     const returnedVans = getVans()
-     return returnedVans
+export function loader() {
+    return getVans()
 }
-
 
 export default function Vans() {
     const [searchParams, setSearchParams] = useSearchParams()
-    const [vans, setVans] = React.useState([])
-    const [loading, setLoading] = React.useState(false)
     const [error, setError] = React.useState(null)
-    
-    const data = useLoaderData()
-    console.log(data)
+    const vans = useLoaderData()
 
     const typeFilter = searchParams.get("type")
-
-    React.useEffect(() => {
-        async function loadVans() {
-            setLoading(true)
-            try {
-                const data = await getVans()
-                setVans(data)
-            } catch(err) {
-                setError(err)
-            } finally {
-                setLoading(false)
-            }
-        }
-        return loadVans()
-
-    }, [])
 
     const displayedVans = typeFilter
         ? vans.filter(van => van.type === typeFilter)
@@ -58,8 +36,6 @@ export default function Vans() {
         </div>
     ))
 
-
-
     function handleFilterChange(key, value) {
         setSearchParams(prevParams => {
             if (value === null) {
@@ -70,14 +46,10 @@ export default function Vans() {
             return prevParams
         })
     }
-    if (loading){
-        return <h1>Loading...</h1>
-    }
-
+    
     if (error) {
         return <h1>There was an error: {error.message}</h1>
     }
-
 
     return (
         <div className="van-list-container">
