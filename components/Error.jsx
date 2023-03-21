a7d61d1cb4054f60eff430396cbc9746a2eea33a
frontendella <ryukhinaella@gmail.com>
2023-03-20 18:09:32 -0700

    added useRouteError hook

--- components/Error.jsx
+++ components/Error.jsx
@@ -1,7 +1,14 @@
 import React from "react"
-export default function Error(){
+import { useRouteError } from "react-router-dom"
 
+export default function Error() {
+    const error = useRouteError()
+    console.log(error)
+    
     return (
-        <h1>Error occured</h1>
+        <>
+            <h1>Error: {error.message}</h1>
+            <pre>{error.status} - {error.statusText}</pre>
+        </>
     )
 }
