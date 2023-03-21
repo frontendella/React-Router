69440376e2bdc11c2e2002cf806e630df212e3ab
frontendella <ryukhinaella@gmail.com>
2023-03-20 21:59:54 -0700

    Authentication for negative use case

--- pages/Login.jsx
+++ pages/Login.jsx
@@ -1,15 +1,40 @@
 import React from "react"
 import { useNavigate, useLocation } from "react-router-dom"
 import { loginUser } from "../api"
+/**
+ * Challenge: Code the sad path 🙁
+ * 5. Add an `error` state and default it to `null`. When the
+ *    form is submitted, reset the errors to `null`. If there's
+ *    an error from `loginUser` (add a .catch(err => {...}) in 
+ *    the promise chain), set the error to the error that 
+ *    comes back.
+ * 6. Display the error.message below the `h1` if there's ever
+ *    an error in state
+ */
 
 export default function Login() {
-    const [loginFormData, setLoginFormData] = React.useState({ email: "", password: "" })
+    const [loginFormData, setLoginFormData] = React.useState(
+        { email: "", password: "" }
+    )
+    const [status, setStatus] = React.useState("idle")
+    const [error, setError] = React.useState(null)
+    
     const location = useLocation()
+    
     function handleSubmit(e) {
         e.preventDefault()
+        setStatus("submitting")
+        setError(null)
         loginUser(loginFormData)
-        .then(data => console.log(data))
-
+            .then(data => {
+                console.log(data)
+            })
+            .catch(err => {
+                setError(err)
+            })
+            .finally(() =>{
+                setStatus("idle")
+            })
     }
 
     function handleChange(e) {
@@ -22,11 +47,15 @@ export default function Login() {
 
     return (
         <div className="login-container">
-             {
+            {
                 location.state?.message && 
-                <h3 className="login-first">{location.state.message}</h3>
+                <h3 className="login-error">{location.state.message}</h3>
             }
             <h1>Sign in to your account</h1>
+            {
+                error && 
+                <h3 className="login-error">{error.message}</h3>
+            }
             <form onSubmit={handleSubmit} className="login-form">
                 <input
                     name="email"
@@ -42,7 +71,14 @@ export default function Login() {
                     placeholder="Password"
                     value={loginFormData.password}
                 />
-                <button>Log in</button>
+                <button 
+                    disabled={status === "submitting"}
+                >
+                    {status === "submitting" 
+                        ? "Logging in..." 
+                        : "Log in"
+                    }
+                </button>
             </form>
         </div>
     )
