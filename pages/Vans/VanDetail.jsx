6918fb6b30b2c0e3ab49fc7fadb3c3fce74230f4
frontendella <ryukhinaella@gmail.com>
2023-03-22 22:21:05 -0700

    Van detail pages: defer, loader, Aware and Suspense added

--- pages/Vans/VanDetail.jsx
+++ pages/Vans/VanDetail.jsx
@@ -1,41 +1,49 @@
-import React from "react"
-import { Link, useParams, useLocation } from "react-router-dom"
+import React, {Suspense} from "react"
+import { Link, useLocation, defer,  Await, useLoaderData } from "react-router-dom"
+import { getVans } from "../../api"
 
+export function loader({params}){
+    return defer ({van: getVans(params.id)})
+}
+
+export function loader({params}) {
+    console.log(params)
+    // Do our loader stuff here
+}
+ 
 export default function VanDetail() {
-    const params = useParams()
     const location = useLocation()
+    const loaderData = useLoaderData()
 
     const [van, setVan] = React.useState(null)
 
-    React.useEffect(() => {
-        fetch(`/api/vans/${params.id}`)
-            .then(res => res.json())
-            .then(data => setVan(data.vans))
-    }, [params.id])
-
-    const search = location.state?.search || ''
-    const type = location.state?.type || "all"
-
+    const search = location.state?.search || "";
+    const type = location.state?.type || "all";
+    
     return (
         <div className="van-detail-container">
-            <Link
-                to={`..${search}`}
-                relative="path"
-                className="back-button"
-            >&larr; <span>Back to {type} vans</span></Link>
-            
-            {van ? (
-                <div className="van-detail">
-                    <img src={van.imageUrl} />
-                    <i className={`van-type  selected`}>
-                        {van.type}
-                    </i>
-                    <h2>{van.name}</h2>
-                     <p className="van-price"><span>${van.price}</span>/day</p>
-                    <p>{van.description}</p>
-                    <button className="link-button">Rent this van</button>
-                </div>
-            ) : <h2>Loading...</h2>}
+                <Link
+                    to={`..${search}`}
+                    relative="path"
+                    className="back-button"
+                >&larr; <span>Back to {type} vans</span></Link>
+            <Suspense fallback={<h2>Loading...</h2>}>
+                <Await resolve={loaderData.van}>
+                    {(van)=>(
+                         <div className="van-detail">
+                         <img src={van.imageUrl} />
+                         <i className={`van-type ${van.type} selected`}>
+                             {van.type}
+                         </i>
+                         <h2>{van.name}</h2>
+                         <p className="van-price"><span>${van.price}</span>/day</p>
+                         <p>{van.description}</p>
+                         <button className="link-button">Rent this van</button>
+                     </div>
+                    )}
+                   
+                </Await>
+            </Suspense>
         </div>
     )
 }
