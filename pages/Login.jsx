bdc017f74f8213f4fa4127ad4f8c470d6ed4421f
frontendella <ryukhinaella@gmail.com>
2023-03-22 22:40:06 -0700

    Added valid Login credentials to the "Login" page as comments

--- pages/Login.jsx
+++ pages/Login.jsx
@@ -18,14 +18,6 @@ export async function action({ request }) {
     }
 }
 
-/**
- * Challenge: Disable the Log in button and change its text to
- * "Logging in..." when the form is in a "submitting" state.
- * 
- * Extra credit: Change the button's background color to "#AAAAAA"
- * and use cursor: not-allowed while the button is in its "disabled"
- * state.
- */
 
 export default function Login() {
     const data = useActionData()
@@ -75,5 +67,7 @@ export default function Login() {
             </Form>
         </div>
     )
-
 }
+// Valid login data: 
+// email: b@b.com
+// password: p123
